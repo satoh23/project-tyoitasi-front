c6e8c16a7177b99e7aefba804e5c331a1b95fa86
tetsuya <satoh1529@gmail.com>
2021-07-16 20:49:37 +0900

    試し

--- api/auth/login.tsx
+++ api/auth/login.tsx
@@ -14,6 +14,6 @@ export async function login(email: string, password: string) {
       isSuccess = true;
     }
   });
-  // window.location.reload();
+  window.location.reload();
   return isSuccess;
 }
