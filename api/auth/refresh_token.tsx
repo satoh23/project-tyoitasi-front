3f175c9049a7e923f43879e763a33f6faa08c304
tetsuya <satoh1529@gmail.com>
2021-07-19 14:14:55 +0900

    トークンリフレッシュのurl変更

--- api/auth/refresh_token.tsx
+++ api/auth/refresh_token.tsx
@@ -1,12 +1,15 @@
 export async function refreshToken() {
   let isSuccess;
-  await fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}api/v1/token-refresh/`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    credentials: "include",
-  }).then((res) => {
+  await fetch(
+    `${process.env.NEXT_PUBLIC_RESTAPI_URL}dj-rest-auth/token/refresh/`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    }
+  ).then((res) => {
     if (res.ok) {
       isSuccess = true;
     } else if (res.status === 500) {
