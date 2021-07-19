f05bcef44e397bab15c55d5eae1deaa400ca9fe5
tetsuya <satoh1529@gmail.com>
2021-07-19 19:52:26 +0900

    ログイン周りの修正

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -13,18 +13,11 @@ const cookie = new Cookies();
 const Layout = ({ children }) => {
   const [nowLogin, setNowLogin] = useState(false);
   useEffect(() => {
-    const author = cookie.get("UID");
-    const isLogin = cookie.get("NLN");
-    console.log(cookie.get("NLN"));
+    let aa = document.cookie.split(";");
+    console.log(aa);
     setNowLogin(cookie.get("UID"));
-    console.log(author);
     refreshToken();
   }, []);
-
-  const aa = async () => {
-    const author = cookie.get("UID");
-    console.log(author);
-  };
 
   return (
     <div>
@@ -39,7 +32,6 @@ const Layout = ({ children }) => {
       <div className="flex justify-center items-center flex-col min-h-screen text-black font-mono">
         <main className="flex flex-1 float-right justify-center items-center flex-col w-full mt-20 lg:mt-24 mb-16 lg:mb-3">
           {children}
-          <button onClick={aa}>dfadsgds</button>
         </main>
         <footer className="w-full h-6 flex justify-center items-center text-gray-500 text-sm"></footer>
       </div>
