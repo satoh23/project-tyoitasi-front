9c1a8b93ea6a44084728fcbf085ea546ea52f829
tetsuya <satoh1529@gmail.com>
2021-07-19 16:07:07 +0900

    ログイン周りの修正

--- components/layout/parts/Navbar.tsx
+++ components/layout/parts/Navbar.tsx
@@ -28,7 +28,7 @@ const Navbar = () => {
           <a className="inline-flex items-center p-1 mr-4 ml-2">
             <path d="M12.001 4.8c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624C13.666 10.618 15.027 12 18.001 12c3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C16.337 6.182 14.976 4.8 12.001 4.8zm-6 7.2c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624 1.177 1.194 2.538 2.576 5.512 2.576 3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C10.337 13.382 8.976 12 6.001 12z" />
             <span className="text-xl text-white tracking-wide">
-              ちょいたし！
+              ちょいたし！{nowLogin}
             </span>
           </a>
         </Link>
@@ -84,7 +84,7 @@ const Navbar = () => {
         </button>
         <aside className="">
           <div className="lg:ml-auto min-w-full w-max flex flex-col lg:h-auto mt-12 lg:mt-14 z-30">
-            {nowLogin.length > 10 ? (
+            {nowLogin > 10 ? (
               <>
                 <Link href="/article/create">
                   <a className="border-b border-pink-200 w-full px-3 py-2 mb-4 mt-4 ml-2 rounded text-yellow-600 hover:bg-hover-yellow hover:text-white">
