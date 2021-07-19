bdd0f7d18e6158ae7c04255eae44b02a48e244f6
tetsuya <satoh1529@gmail.com>
2021-07-19 20:15:19 +0900

    ログイン周りの修正

--- api/auth/refresh_token.tsx
+++ api/auth/refresh_token.tsx
@@ -1,20 +1,24 @@
 export async function refreshToken() {
   let isSuccess;
-  await fetch(
-    `${process.env.NEXT_PUBLIC_RESTAPI_URL}dj-rest-auth/token/refresh/`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      credentials: "include",
-    }
-  ).then((res) => {
-    if (res.ok) {
-      isSuccess = true;
-    } else if (res.status === 500) {
-      isSuccess = false;
-    }
-  });
+  try {
+    await fetch(
+      `${process.env.NEXT_PUBLIC_RESTAPI_URL}dj-rest-auth/token/refresh/`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+      }
+    ).then((res) => {
+      if (res.ok) {
+        isSuccess = true;
+      } else if (res.status === 500) {
+        isSuccess = false;
+      }
+    });
+  } catch {
+    isSuccess = false;
+  }
   return isSuccess;
 }
