7ea4a0eb93ba7581e5551ca544b8e0326c549d4d
tetsuya <satoh1529@gmail.com>
2021-07-23 23:35:13 +0900

    UIのへんこう

--- components/layout/parts/Navbar.tsx
+++ components/layout/parts/Navbar.tsx
@@ -82,8 +82,21 @@ const Navbar = ({ nowLogin }) => {
           <div className="lg:ml-auto min-w-full w-max flex flex-col lg:h-auto mt-12 lg:mt-14 z-30">
             {nowLogin ? (
               <>
-                <Link href="/article/create">
+                <Link href="/">
                   <a className="border-b border-border-yellow w-full px-5 py-2 mb-4 mt-4 rounded text-yellow-600 hover:bg-hover-yellow hover:text-white">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6 inline-block pb-1 mr-1"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
+                    </svg>
+                    ホーム
+                  </a>
+                </Link>
+                <Link href="/article/create">
+                  <a className="border-b border-border-yellow w-full px-5 py-2 mb-4 rounded text-yellow-600 hover:bg-hover-yellow hover:text-white">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       className="h-6 w-6 inline-block pb-1 mr-1"
@@ -139,28 +152,43 @@ const Navbar = ({ nowLogin }) => {
                 </button>
               </>
             ) : (
-              <button
-                className="min-w-max hover:bg-nav-yellow mb-6"
-                onClick={() => router.push("/auth/auth")}
-              >
-                <div className="border-b border-border-yellow ml-2 px-3 py-2 rounded text-yellow-600 hover:text-white hover:bg-nav-yellow text-left">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-6 w-6 inline-block pb-1 ml-1"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
-                    />
-                  </svg>
-                  ログイン
-                </div>
-              </button>
+              <>
+                <Link href="/">
+                  <a className="border-b border-border-yellow w-full px-5 py-2 mb-4 mt-4 rounded text-yellow-600 hover:bg-hover-yellow hover:text-white">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6 inline-block pb-1 mr-1"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
+                    </svg>
+                    ホーム
+                  </a>
+                </Link>
+                <button
+                  className="min-w-max hover:bg-nav-yellow"
+                  onClick={() => router.push("/auth/auth")}
+                >
+                  <div className="border-b border-border-yellow ml-2 px-3 py-2 rounded text-yellow-600 hover:text-white hover:bg-nav-yellow text-left">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6 inline-block pb-1 ml-1"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+                      />
+                    </svg>
+                    ログイン
+                  </div>
+                </button>
+              </>
             )}
           </div>
         </aside>
