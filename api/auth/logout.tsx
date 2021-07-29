dc3fd42801443143d4243aa5548c1405ea1c0790
tetsuya <satoh1529@gmail.com>
2021-07-29 14:11:20 +0900

    ユーザー編集画面

--- api/auth/logout.tsx
+++ api/auth/logout.tsx
@@ -1,55 +1,30 @@
 export async function logout() {
-  let haveValidToken = false;
-  let wasGetNewToken = false;
-  try {
-    await fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}api/v1/logout/`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      credentials: "include",
-    }).then((res) => {
-      if (res.status === 401) {
-        haveValidToken = false;
-      } else if (res.ok) {
-        haveValidToken = true;
-      }
-    });
-  } catch (err) {
-    alert(err);
-  }
-  try {
-    if (!haveValidToken) {
-      await fetch(
-        `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/v1/token-refresh/`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          credentials: "include",
-        }
-      ).then((res) => {
-        if (res.ok) {
-          wasGetNewToken = true;
-        }
-      });
-    }
-  } catch (err) {
-    alert(err);
-  }
-  try {
-    if (!wasGetNewToken) {
-      await fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}api/v1/logout/`, {
+  await fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}api/v1/logout/`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+  }).then((res) => {
+    if (res.status === 401) {
+      fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}api/v1/token-refresh/`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         credentials: "include",
+      }).then((res) => {
+        if (res.ok) {
+          fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}api/v1/logout/`, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            credentials: "include",
+          });
+        }
       });
     }
-  } catch (err) {
-    alert(err);
-  }
+  });
   window.location.reload();
 }
