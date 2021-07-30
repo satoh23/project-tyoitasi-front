2b56a4077eb086a4e1246c2369a4f6b156c1bcda
tetsuya <satoh1529@gmail.com>
2021-07-30 21:04:03 +0900

    サイト説明の追加

--- components/index/Description.js
+++ components/index/Description.js
@@ -1,6 +1,5 @@
 import React from "react";
 import dynamic from "next/dynamic";
-import Image from 'next/image'
 
 const Carousel = dynamic(() => import("@brainhubeu/react-carousel"), {
   ssr: false,
@@ -10,12 +9,7 @@ const Description = () => {
   return (
     <div className="w-full text-center font-bold text-2xl m-5">
       <Carousel plugins={["arrows"]}>
-        <Image
-            src="/NoImage.jpg"
-            alt="icon" 
-            className="rounded-lg"
-            width={170}
-            height={170}/>
+        <img src="/NoImage.jpg"/>
       </Carousel>
       <div className="mb-5">
         インスタント食品に1品だけ足して美味しくするサイト
