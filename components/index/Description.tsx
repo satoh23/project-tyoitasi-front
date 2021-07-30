d84209a73ded687bfd68994a989f0afa4652e7c5
tetsuya <satoh1529@gmail.com>
2021-07-30 20:43:06 +0900

    サイト説明の追加

--- components/index/Description.tsx
+++ components/index/Description.tsx
@@ -2,10 +2,11 @@ import React from "react";
 
 const Description = () => {
   return (
-    <div className="w-full text-center font-bold text-lg">
-      インスタント食品に1品だけ足して美味しくするサイト
-      <br />
-      ちょいたし！
+    <div className="w-full text-center font-bold text-5xl m-5">
+      <div className="mb-5">
+        インスタント食品に1品だけ足して美味しくするサイト
+      </div>
+      <div className="text-yellow-300">ちょいたし！</div>
     </div>
   );
 };
