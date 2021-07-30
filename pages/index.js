2138a47e5d7a9adbb9218e79de59c838ee1452dd
tetsuya <satoh1529@gmail.com>
2021-07-30 15:44:24 +0900

    フィルターの追加

--- pages/index.js
+++ pages/index.js
@@ -61,19 +61,17 @@ export default function Home({ staticArticles }) {
 
     return (
         <Layout>
-            <form onSubmit={filteringArticle}>
-                <div className="flex w-full">
-                    <span className="w-5/12 md:w-3/12 mt-1 text-sm">検索</span>
-                    <input
-                    required
-                    className="mb-5 mr-6 sm:mr-0 appearance-none rounded-none relative inline-flex w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md rounded-b-md focus:outline-none focus:ring-yellow-400 focus:border-yellow-400 text-sm"
-                    type="text"
-                    placeholder="サッポロ一番"
-                    maxLength="50"
-                    value={mainMaterial}
-                    onChange={(e) => setMainMaterial( e.target.value )}
-                    />
-                </div>
+            <form onSubmit={filteringArticle} className="flex w-full">
+                <span className="w-5/12 md:w-3/12 mt-1 text-sm">検索</span>
+                <input
+                required
+                className="mb-5 mr-6 sm:mr-0 appearance-none rounded-none relative inline-flex w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md rounded-b-md focus:outline-none focus:ring-yellow-400 focus:border-yellow-400 text-sm"
+                type="text"
+                placeholder="サッポロ一番"
+                maxLength="50"
+                value={mainMaterial}
+                onChange={(e) => setMainMaterial( e.target.value )}
+                />
                 <button
                 type="submit"
                 className="mr-3 ml-5 bg-transparent font-bold bg-yellow-300 hover:bg-white text-white hover:text-yellow-300 hover:text-white py-2 px-3 rounded"
