7bc41eccde911b4b24c1bb991a465a9fca5a9d4e
tetsuya <satoh1529@gmail.com>
2021-07-30 16:27:16 +0900

    フィルターの追加

--- pages/index.js
+++ pages/index.js
@@ -62,10 +62,10 @@ export default function Home({ staticArticles }) {
     return (
         <Layout>
             <form onSubmit={filteringArticle} className="flex w-full justify-center justify-items-center">
-                <span className="w-5/12 md:w-3/12 mt-1 mr-2 text-sm">検索:</span>
+                <span className="mt-1 mr-2 text-sm">検索:</span>
                 <input
                 required
-                className="w-10/12 appearance-none rounded-none relative inline-flex px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md rounded-b-md focus:outline-none focus:ring-yellow-400 focus:border-yellow-400 text-sm"
+                className="w-2/3 appearance-none rounded-none relative inline-flex px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md rounded-b-md focus:outline-none focus:ring-yellow-400 focus:border-yellow-400 text-sm"
                 type="text"
                 placeholder="サッポロ一番"
                 maxLength="50"
