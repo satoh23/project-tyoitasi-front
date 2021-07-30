e0130ac7798da099119ec7853ca664db428e0e81
tetsuya <satoh1529@gmail.com>
2021-07-30 16:09:06 +0900

    フィルターの追加

--- pages/index.js
+++ pages/index.js
@@ -66,7 +66,7 @@ export default function Home({ staticArticles }) {
                     <span className="w-5/12 md:w-3/12 mt-1 text-sm">検索</span>
                     <input
                     required
-                    className="mr-6 sm:mr-0 appearance-none rounded-none relative inline-flex w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md rounded-b-md focus:outline-none focus:ring-yellow-400 focus:border-yellow-400 text-sm"
+                    className="mr-6 sm:mr-0 appearance-none rounded-none relative inline-flex w-7/12 md:w-9/12 px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md rounded-b-md focus:outline-none focus:ring-yellow-400 focus:border-yellow-400 text-sm"
                     type="text"
                     placeholder="サッポロ一番"
                     maxLength="50"
@@ -76,7 +76,7 @@ export default function Home({ staticArticles }) {
                 </span>
                 <button
                 type="submit"
-                className="bg-transparent font-bold bg-yellow-300 hover:bg-white text-white hover:text-yellow-300 hover:text-white px-3 rounded"
+                className="bg-transparent font-bold bg-yellow-300 hover:bg-white text-white hover:text-yellow-300 hover:text-white rounded"
                 >
                     検索する
                 </button>
